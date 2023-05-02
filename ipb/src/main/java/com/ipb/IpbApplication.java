9dab3464a7ea1dce9e3c01a36670e1665f5b785c
채란 <122114197+luckyKite@users.noreply.github.com>
2023-05-02 10:49:00 +0900

    error test

--- ipb/src/main/java/com/ipb/IpbApplication.java
+++ ipb/src/main/java/com/ipb/IpbApplication.java
@@ -15,7 +15,7 @@ public class IpbApplication {
 		System.out.println("다시 테스트 합니당");
 		System.out.println("밍디냠냠");
 		System.out.println("ㅜㅜㅜㅜㅜㅜ");
-
+		System.out.println("브랜치를 만들어야합니다.");
 	}
 
 }
